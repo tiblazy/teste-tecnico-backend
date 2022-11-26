8e623d12da58e2d3706c16e1331a93062d8e3867
tiblazy <hauntedapply@gmail.com>
2022-11-26 01:29:24 -0300

    feat: configuração docker

--- _project/settings.py
+++ _project/settings.py
@@ -2,11 +2,6 @@ from pathlib import Path
 import os
 import environ
 import dj_database_url
-# import django_on_heroku
-
-# Django on Heroku
-# django_on_heroku.settings(locals())
-
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
