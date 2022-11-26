f234131c1b86a2920cfe1e16d6afd43b0b83f512
tiblazy <hauntedapply@gmail.com>
2022-11-25 23:14:37 -0300

    feat: lista de transações, alteração de retorno tipo (numérico) para (texto)

--- transactions/serializers.py
+++ transactions/serializers.py
@@ -2,15 +2,18 @@ from rest_framework import serializers
 
 from .models import Transaction
 
-from uploads.utils import calculate_store_currency
+from uploads.utils import transactions_store, calculate_store_currency
 
 
 class TransactionSerializer(serializers.ModelSerializer):
+    transactions = serializers.SerializerMethodField()
     currency = serializers.SerializerMethodField()
-
     class Meta:
         model = Transaction
-        fields = ["store", "currency"]
+        fields = ["store", "currency", "transactions"]
 
+    def get_transactions(self, store):
+        return transactions_store(store)
+    
     def get_currency(self, store):
         return calculate_store_currency(store)
--- uploads/utils.py
+++ uploads/utils.py
@@ -1,6 +1,5 @@
 from transactions.models import Transaction
 
-
 def normalize_file_txt(file) -> dict:
     decode_file = file.read().decode("utf-8")
     split_file = decode_file.split("\r\n")
@@ -19,8 +18,7 @@ def normalize_file_txt(file) -> dict:
 
         Transaction.objects.create(**data)
 
-
-def filter_store(stores) -> float:
+def filter_store(stores) -> list:
     store_names = []
     store_unique = []
 
@@ -34,10 +32,42 @@ def filter_store(stores) -> float:
 
     return store_unique
 
+def get_store(store_name) -> list:
+    store = list(store_name.values())[0]
+    return Transaction.objects.filter(store=store)
+
+def transactions_store(store_name) -> list:
+    stores = get_store(store_name)
+    transactions = []
+    transactions_type = [
+        {"1":"Debit"}, 
+        {"2":"Bank Slip"}, 
+        {"3":"Financing"}, 
+        {"4":"Credit"}, 
+        {"5":"Loan"},
+        {"6":"Sell"},
+        {"7":"TED"}, 
+        {"8":"DOC"},
+        {"9":"Rent"},
+                        ]
+    negative = [
+        "2",
+        "3",
+        "9",
+    ]
+    
+    for transaction in stores:
+        for trasaction_type in transactions_type:
+            if list(trasaction_type.keys())[0] == transaction.type:
+                if transaction.type in negative:
+                    transactions.append({"type": list(trasaction_type.values())[0], "value": round(-transaction.value,2),},)
+                else:
+                    transactions.append({"type": list(trasaction_type.values())[0], "value": round(transaction.value,2),},)
+
+    return transactions
 
 def calculate_store_currency(store_name) -> float:
-    store = list(store_name.values())[0]
-    stores = Transaction.objects.filter(store=store)
+    stores = get_store(store_name)
     negative = [
         "2",
         "3",
